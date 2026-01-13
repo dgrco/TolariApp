21497f2446c3dc2b79eb27541a99a47d25eabf38
Dante Grieco <dantegrieco11@gmail.com>
2026-01-13 15:56:40 -0800

    Fixed npm vulnerabilities in frontend via 'npm audit fix'

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ var assets embed.FS
 func main() {
 	// Create an instance of the app structure
 	app := NewApp()
- 
+
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "Tolari",
@@ -24,9 +24,9 @@ func main() {
 			Assets: assets,
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup: app.startup,
-		OnShutdown: app.shutdown,
-		Bind: []interface{}{
+		OnStartup:        app.startup,
+		OnShutdown:       app.shutdown,
+		Bind: []any{
 			app,
 		},
 	})
